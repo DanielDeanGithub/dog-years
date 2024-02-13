0e56ade6570d6f1e6dec191412aafcf85c952a4c
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-13 09:31:58 +0000

    Added laterYears variable and included comment for it

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// my current age (when getting out of bed first thing in the morning)
+// My current age (when getting out of bed first thing in the morning)
 const myAge = 100;
 
-// this variable will change
+// This variable will change
 let earlyYears = 2;
 
 earlyYears *= 10.5;
+
+// We already accounted for the first two years so take the myAge variable, and subtract 2 from it
+let laterYears = myAge - 2;
