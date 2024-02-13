51dc0ddd09eb966083561cbcb1370f442c44d98d
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-13 09:34:24 +0000

    Multiplied laterYears by 4 and added comment

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const myAge = 100;
 
 // This variable will change
 let earlyYears = 2;
-
 earlyYears *= 10.5;
 
 // We already accounted for the first two years so take the myAge variable, and subtract 2 from it
 let laterYears = myAge - 2;
+
+// Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by later years.
+laterYears *= 4;
